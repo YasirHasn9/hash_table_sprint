b07d1536554436246d1310e20f544da4d7baa49c
YasirHasn9 <hassanyasir223@gmail.com>
2020-08-07 09:41:04 -0700

    ex4 has_negatives

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,6 +3,22 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    # if you wanna get fired then do this lol
+    # result = []
+    # for i in range(len(a)-1):
+    #     for j in range(i + 1 , len(a)):
+    #         if a[i] + a[j] == 0:
+    #             result.append(abs(a[i]))
+
+    
+    result = []
+    # add each number to d
+    # access a key/value on obj is O(1)
+    d_numbers = {}
+    for num in a:
+        d_numbers[num] = 1
+        if num != 0 and -num in d_numbers:
+            result.append(abs(num))
 
     return result
 
